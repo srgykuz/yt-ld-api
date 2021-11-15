39346c6013a6cdc7956d7913313c4dee5acf4277
Sergey Kuznetsov <amatsukine@gmail.com>
2021-11-15 21:42:19 +0300

    Add tests for server/middleware.go

---
+++ server/middleware_test.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLogReqResMiddleware(t *testing.T) {
+	wantRedirectPath := "/redirect"
+	wantStatusCode := http.StatusMovedPermanently
+
+	redirH := http.RedirectHandler(wantRedirectPath, wantStatusCode)
+	middH := logReqResMiddleware(redirH)
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	middH.ServeHTTP(w, req)
+
+	if w.Code != wantStatusCode {
+		t.Errorf("status code = %v, want = %v", w.Code, wantStatusCode)
+	}
+
+	if l := w.Header().Get("Location"); l != wantRedirectPath {
+		t.Errorf("redirect location = %s, want = %s", l, wantRedirectPath)
+	}
+}
