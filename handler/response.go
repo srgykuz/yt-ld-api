srgykuz/yt-ld-api

commit c04d35077cfff247b0b6a89e077a25fb40110d14

nil response will be not written in response body

handler/response.go
@@ -30,12 +30,13 @@ type response struct {
 }
 
 func (r *response) write(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(r.status)
+	if r.Result != nil {
+		w.Header().Set("Content-Type", "application/json")
 
-	err := encodeResponseBody(w, r)
-
-	if err != nil {
-		logger.Info("unable to encode response body: " + err.Error())
+		if err := encodeResponseBody(w, r); err != nil {
+			logger.Info("unable to encode response body: " + err.Error())
+		}
 	}
+
+	w.WriteHeader(r.status)
 }

commit 94b3282428e5725421de20e5d381e9d293e3fd10

response: fix a bug when status code can't be written with non-nil body
This bug was introduced by c04d35077cfff247b0b6a89e077a25fb40110d14

handler/response.go
@@ -32,11 +32,13 @@ type response struct {
 func (r *response) write(w http.ResponseWriter) {
 	if r.Result != nil {
 		w.Header().Set("Content-Type", "application/json")
+	}
 
+	w.WriteHeader(r.status)
+
+	if r.Result != nil {
 		if err := encodeResponseBody(w, r); err != nil {
 			logger.Info("unable to encode response body: " + err.Error())
 		}
 	}
-
-	w.WriteHeader(r.status)
 }